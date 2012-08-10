c12404f1606432c36203c60a5d6bd4480262e130
wixner <wixner@0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c>
2012-08-10 10:13:36 +0000

    Uses a named datasource binding to allow several datasources to coexist in an injector. Also remove lots of code creating the guice module and reading properties in favor over passing a module instance directly to the constructor
    git-svn-id: https://gbif-common-resources.googlecode.com/svn/gbif-common-test/trunk@955 0ccb6880-a3f6-2ae5-22e0-ac1ed81bfb6c

--- src/main/java/org/gbif/test/DatabaseDrivenTestRule.java
+++ src/main/java/org/gbif/test/DatabaseDrivenTestRule.java
@@ -1,28 +1,24 @@
 package org.gbif.test;
 
+
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Constructor;
 import java.sql.Connection;
-import java.util.Enumeration;
 import java.util.Map;
-import java.util.Properties;
 import javax.annotation.Nullable;
 import javax.sql.DataSource;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.Closeables;
 import com.google.common.io.Resources;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Key;
 import com.google.inject.Module;
+import com.google.inject.name.Names;
 import com.jolbox.bonecp.BoneCPDataSource;
 import liquibase.Liquibase;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
-import org.apache.commons.io.FileUtils;
 import org.dbunit.DefaultDatabaseTester;
 import org.dbunit.database.DatabaseDataSourceConnection;
 import org.dbunit.database.IDatabaseConnection;
@@ -37,62 +33,40 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 
 /**
  * A TestRule for Database driven Integration tests.
- * This sets up Guice, Liquibase and DbUnit.
+ * This sets up Guice, Liquibase and DbUnit and exposes the guice injector and the optionally given main service class
+ * for quick testing.
  */
 public class DatabaseDrivenTestRule<T> implements TestRule {
 
   protected final Logger log = LoggerFactory.getLogger(getClass());
   private static final String[] LIQUIBASE_FILES = {"master.xml"};
 
-  private File tempDir;
   private Connection connection;
   private DefaultDatabaseTester databaseTester;
   protected DataSource dataSource;
   private final Class<T> serviceClass;
   private T service;
+  private Injector injector;
 
-  private final Class<? extends Module> moduleClass;
-  private final Properties properties;
+  private final Module module;
   private final String dbUnitFileName;
   private final Map<String, Object> dbUnitProperties;
+  private final String datasourceBindingName;
 
   /**
-   * @param moduleClass    the class of the Guice Module to use
-   * @param serviceClass   the class for the service we want to wire up and test
-   * @param propertiesFile the properties file to read the configuration details from
-   * @param propertyPrefix the prefix used to filter the properties. E.g.
-   *                       {@code occurrencestore.db}
-   * @param dbUnitFileName the optional unqualified filename within the dbUnit package to be used in setting up the db
-   *
-   * @deprecated use the properties based constructor instead to harmonize all methods using properties
+   * @param module                the Guice Module to use to create the exposed injector
+   * @param datasourceBindingName the name of the named Datasource guice binding to use
+   * @param serviceClass          the class for the service we want to wire up and test
+   * @param dbUnitFileName        the optional unqualified filename within the dbUnit package to be used in setting up
+   *                              the db
    */
-  @Deprecated
-  public DatabaseDrivenTestRule(Class<? extends Module> moduleClass, @Nullable Class<T> serviceClass,
-    String propertiesFile, String propertyPrefix, @Nullable String dbUnitFileName,
-    Map<String, Object> dbUnitProperties) {
-    // TODO: can we use annotations to change the dbunit file name on every test method?
+  public DatabaseDrivenTestRule(Module module, String datasourceBindingName, @Nullable Class<T> serviceClass,
+    @Nullable String dbUnitFileName, Map<String, Object> dbUnitProperties) {
     this.dbUnitFileName = dbUnitFileName;
     this.serviceClass = serviceClass;
-    this.moduleClass = moduleClass;
+    this.module = module;
     this.dbUnitProperties = ImmutableMap.copyOf(dbUnitProperties);
-
-    this.properties = loadProperties(propertiesFile, propertyPrefix);
-  }
-
-  /**
-   * @param moduleClass    the class of the Guice Module to use
-   * @param serviceClass   the class for the service we want to wire up and test
-   * @param properties     the properties to use as configuration details with prefixes if existing
-   * @param dbUnitFileName the optional unqualified filename within the dbUnit package to be used in setting up the db
-   */
-  public DatabaseDrivenTestRule(Class<? extends Module> moduleClass, @Nullable Class<T> serviceClass,
-    Properties properties, @Nullable String dbUnitFileName, Map<String, Object> dbUnitProperties) {
-    this.properties = properties;
-    // TODO: can we use annotations to change the dbunit file name on every test method?
-    this.dbUnitFileName = dbUnitFileName;
-    this.serviceClass = serviceClass;
-    this.moduleClass = moduleClass;
-    this.dbUnitProperties = ImmutableMap.copyOf(dbUnitProperties);
+    this.datasourceBindingName = datasourceBindingName;
   }
 
   @Override
@@ -115,10 +89,10 @@ public class DatabaseDrivenTestRule<T> implements TestRule {
     SLF4JBridgeHandler.install();
 
     // create private guice module with properties passed to constructor
-    Constructor<? extends Module> c = moduleClass.getConstructor(Properties.class);
-    Injector injector = Guice.createInjector(c.newInstance(properties));
+    injector = Guice.createInjector(module);
 
-    dataSource = injector.getInstance(DataSource.class);
+    Key<DataSource> regDatasourceKey = Key.get(DataSource.class, Names.named(datasourceBindingName));
+    dataSource = injector.getInstance(regDatasourceKey);
     service = serviceClass == null ? null : injector.getInstance(serviceClass);
 
     connection = dataSource.getConnection();
@@ -136,38 +110,6 @@ public class DatabaseDrivenTestRule<T> implements TestRule {
 
   }
 
-  /**
-   * Tries to read a properties file from the class path.
-   *
-   * @param propertiesFile to read
-   *
-   * @return loaded properties
-   *
-   * @throws IOException if the file can't be read
-   * @deprecated to be removed with the deprecated constructor
-   */
-  @Deprecated
-  private Properties loadProperties(String propertiesFile, String propertyPrefix) {
-    InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propertiesFile);
-    Properties properties = new Properties();
-    try {
-      Properties propertiesRaw = new Properties();
-      propertiesRaw.load(inputStream);
-      Enumeration e = propertiesRaw.propertyNames();
-      while (e.hasMoreElements()) {
-        String key = (String) e.nextElement();
-        if (key.startsWith(propertyPrefix)) {
-          properties.put(key.substring(propertyPrefix.length()), propertiesRaw.get(key));
-        }
-      }
-    } catch (IOException e) {
-      throw new RuntimeException("Failed to read properties from file " + propertiesFile, e);
-    } finally {
-      Closeables.closeQuietly(inputStream);
-    }
-    return properties;
-  }
-
   private void after() throws Exception {
     if (databaseTester != null) {
       databaseTester.onTearDown();
@@ -178,10 +120,6 @@ public class DatabaseDrivenTestRule<T> implements TestRule {
 
     if (dataSource instanceof BoneCPDataSource) {
       ((BoneCPDataSource) dataSource).close();
-    }
-
-    if (tempDir != null && tempDir.exists()) {
-      FileUtils.deleteDirectory(tempDir);
     }
   }
 
@@ -216,7 +154,17 @@ public class DatabaseDrivenTestRule<T> implements TestRule {
     }
   }
 
+  /**
+   * @return the service instance for T or null if no serviceClass was passed to the constructor
+   */
   public T getService() {
     return service;
+  }
+
+  /**
+   * @return the guice injector created in this rule. New instance for every test method being executed
+   */
+  public Injector getInjector() {
+    return injector;
   }
 }
